a16e7378e4450294013a2ba2d7951e70be34b148
Scott Macdonald <scottm@bed.link>
2019-01-25 23:34:46 -0500

    output data aa Mbps

--- docker/py-speedtest/run.sh
+++ docker/py-speedtest/run.sh
@@ -6,6 +6,8 @@ while :; do
 	DOWNLOAD=$(echo "${JSON}" | jq '.download')
 	UPLOAD=$(echo "$JSON" | jq '.upload')
 	PING=$(echo "${JSON}" | jq '.ping')
+	DOWNLOAD=$(echo $DOWNLOAD / 1024 / 1024 | bc)
+	UPLOAD=$(echo $UPLOAD / 1024 / 1024 | bc)
 	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}"
 	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${SPEEDTEST_HOST} value=${DOWNLOAD}"
 	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${SPEEDTEST_HOST} value=${UPLOAD}"
